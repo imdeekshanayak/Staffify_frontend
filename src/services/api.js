6c7ccf4f7091357dbd5841b2afc9a6e6df3829cb
Aishwarya Singh <aishwaryasinghbh30@gmail.com>
2026-02-13 00:04:51 +0530

    employee

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // ✅ MUST be 5000
+  baseURL: "https://staffify-backend-1.onrender.com", // ✅ MUST be 5000
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
